8b3e2a98c2bf5653670411ad7bf53ed296177ab5
chaouielmehdi <https://chaouielmehdi@github.com>
2022-02-07 17:45:23 +0100

    add TestClass to master

---
+++ src/test/java/com/example/SpringDemoApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringDemoApplicationTest {
+    @Test
+    public void testLoggerFactory() {
+        System.out.println("------------------------------------------------");
+        System.out.println("------------------------------------------------");
+    }
+}
