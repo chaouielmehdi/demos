952ffffbb0a8d66b20347d7a20641db2889ffed8
elmehdichaoui <chaouielmehdii@gmail.com>
2023-07-09 02:14:38 +0100

    add remove cypress-tls

--- cypress-tls/cypress/e2e/search-in-google.cy.js
+++
@@ -1,6 +0,0 @@
-describe('search in google', () => {
-    it('visit google and search for something', () => {
-        cy.visit('https://www.google.com/')
-        cy.get('.gLFyf').type("test search").type('{enter}')
-    })
-})
